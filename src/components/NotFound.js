d45b0df6a063b77d47205f737c5c4d5874e22010
mhmeroglu <mhmeroglu@gmail.com>
2022-09-01 15:59:09 +0300

    edited

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -5,7 +5,7 @@ const NotFound = () => {
         <div className="not-found">
             <h2>Sorry</h2>
             <p>That page cannot be found</p>
-            <Link to='/'> Back to Homepage</Link>
+            <Link to='/nutritive-values/'> Back to Homepage</Link>
         </div>
     );
 }
